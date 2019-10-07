8ad23c108e22e527c19c4b7c663eee2206f80073
Darin Krauss <darinkrauss@gmail.com>
2019-10-07 09:17:21 -0700

    Initial implementation of Loggly service (#2)
    * Initial implementation of Loggly service
    
    - Implement Service and ServiceUI protocols
    - Implement Logging protocol
    - Connect with Loggly third-party service

---
+++ LogglyServiceKitUI/LogglyService+UI.swift
@@ -0,0 +1,23 @@
+//
+//  LogglyService+UI.swift
+//  LogglyServiceKitUI
+//
+//  Created by Darin Krauss on 6/20/19.
+//  Copyright © 2019 LoopKit Authors. All rights reserved.
+//
+
+import LoopKit
+import LoopKitUI
+import LogglyServiceKit
+
+extension LogglyService: ServiceUI {
+
+    public static func setupViewController() -> (UIViewController & ServiceSetupNotifying & CompletionNotifying)? {
+        return ServiceViewController(rootViewController: LogglyServiceTableViewController(service: LogglyService(), for: .create))
+    }
+
+    public func settingsViewController() -> (UIViewController & ServiceSetupNotifying & CompletionNotifying) {
+      return ServiceViewController(rootViewController: LogglyServiceTableViewController(service: self, for: .update))
+    }
+
+}

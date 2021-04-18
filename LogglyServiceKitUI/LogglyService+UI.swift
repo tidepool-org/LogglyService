b7966c28f85bac854f181b1fc0771aae5fcccaf1
Darin Krauss <darin@tidepool.org>
2021-04-18 10:13:40 -0700

    [LOOP-3375] Onboarding service integration
    - https://tidepool.atlassian.net/browse/LOOP-3375
    - Update to latest Service protocol

--- LogglyServiceKitUI/LogglyService+UI.swift
+++ LogglyServiceKitUI/LogglyService+UI.swift
@@ -16,14 +16,14 @@ extension LogglyService: ServiceUI {
     
     public static var image: UIImage? { nil }
 
-    public static func setupViewController(colorPalette: LoopUIColorPalette) -> SetupUIResult<UIViewController & ServiceCreateNotifying & ServiceOnboardNotifying & CompletionNotifying, ServiceUI>
+    public static func setupViewController(colorPalette: LoopUIColorPalette) -> SetupUIResult<ServiceViewController, ServiceUI>
     {
-        return .userInteractionRequired(ServiceViewController(rootViewController: LogglyServiceTableViewController(service: LogglyService(), for: .create)))
+        return .userInteractionRequired(ServiceNavigationController(rootViewController: LogglyServiceTableViewController(service: LogglyService(), for: .create)))
     }
     
-    public func settingsViewController(colorPalette: LoopUIColorPalette) -> (UIViewController & ServiceOnboardNotifying & CompletionNotifying)
+    public func settingsViewController(colorPalette: LoopUIColorPalette) -> ServiceViewController
     {
-        return ServiceViewController(rootViewController: LogglyServiceTableViewController(service: self, for: .update))
+        return ServiceNavigationController(rootViewController: LogglyServiceTableViewController(service: self, for: .update))
     }
     
     public func supportMenuItem(supportInfoProvider: SupportInfoProvider, urlHandler: @escaping (URL) -> Void) -> AnyView? {
